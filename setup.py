3ecd17ae9f6f19163bc3e3a87313371aa6a3ccfa
jiayibing2333 <jiayibing2333@163.com>
2020-07-15 00:58:30 +0800

    a

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-jia-ming", # Replace with your own username
+    name="pakaging_dongjinglu669", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
