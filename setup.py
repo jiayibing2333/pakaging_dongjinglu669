23bb41f76cb15a324b84913dd4757edf6a75dc03
jiayibing2333 <jiayibing2333@163.com>
2020-07-15 01:22:36 +0800

    a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pakaging_dongjinglu669", # Replace with your own username
-    version="0.0.1",
+    version="0.0.3",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
